ef64bb249f4ad42dace4d16c0a4c0e217f11d3b2
testertinhtoo <kadaeaung4@gmail.com>
2024-07-29 18:55:52 +0700

    Update change in weather controller  in array

--- ToDoListProject/Controllers/WeatherForecastController.cs
+++ ToDoListProject/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Yangon","Madalay"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Yangon","Madalay","Naypyidaw"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
